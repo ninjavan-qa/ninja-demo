80003324e78b21637680a3c6563a7cf0d0993697
Daniel Joi Partogi Hutapea <daniel.joi.ph@gmail.com>
2019-04-01 14:54:14 +0700

    - Update dependencies version. - Add Java class that later can be used to test new QA Engineer candidate.

---
+++ src/test/java/co/nvqa/ninja_demo/exercise/Java8Stream.java
@@ -0,0 +1,16 @@
+package co.nvqa.ninja_demo.exercise;
+
+/**
+ * @author Daniel Joi Partogi Hutapea
+ */
+public class Java8Stream
+{
+    public static void main(String[] args)
+    {
+        /*
+          Use Java 8 Stream to remove all odd numbers
+          and print only number that can divisible by 4.
+         */
+        int[] listOfId = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    }
+}
---
+++ src/test/java/co/nvqa/ninja_demo/exercise/SeleniumRunner.java
@@ -0,0 +1,12 @@
+package co.nvqa.ninja_demo.exercise;
+
+/**
+ * @author Daniel Joi Partogi Hutapea
+ */
+public class SeleniumRunner
+{
+    public static void main(String[] args)
+    {
+        // Create code to automate login page.
+    }
+}
